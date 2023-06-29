10d60cdc0917ef14de64adf85c7c7ef4ab23dcd4
Yohai Wiener <125396366+Monkhai@users.noreply.github.com>
2023-06-29 12:58:37 +0300

    added inifinite scrolling

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Box, Button, Grid, GridItem, Spinner, Text } from '@chakra-ui/react';
 import AppCard from '../components/AppCard';
 import { colors } from '../utilities/colors';
 import CardSkeleton from '../components/CardSkeleton';
 import useGetArtwork, { Art } from '../hooks/useGetArtwork';
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const skeletons = Array(20)
   .fill({})
@@ -14,66 +15,49 @@ interface Props {
 }
 
 const HomeScreen = ({ onArtClick }: Props) => {
-  const { data, error, isLoading, isFetchingNextPage, fetchNextPage } = useGetArtwork();
+  const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useGetArtwork();
+  const fetchedArtCount =
+    data?.pages.reduce((acc, page) => acc + page._embedded.artworks.length, 0) || 0;
 
   if (error) return <Text>{error.message}</Text>;
-
-  if (isLoading) {
-    return (
-      <Box bgGradient={colors.primaryGraidient} h={'auto'} minHeight={'100vh'}>
-        <Grid
-          overflowY={'scroll'}
-          templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
-          templateRows="repeat(auto-fill, minmax(auto, 1fr))"
-          columnGap={'3.5%'}
-          rowGap={5}
-          justifyItems="center"
-          padding={'3%'}
-        >
-          {skeletons.map((Skeleton) => (
-            <CardSkeleton key={Skeleton.id} />
-          ))}
-        </Grid>
-      </Box>
-    );
-  }
 
   return (
     <>
       <Box bgGradient={colors.primaryGraidient} h={'auto'} minHeight={'100vh'}>
-        <Grid
-          overflowY={'scroll'}
-          templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
-          templateRows="repeat(auto-fill, minmax(auto, 1fr))"
-          columnGap={'3.5%'}
-          rowGap={5}
-          justifyItems="center"
-          padding={'3%'}
+        <InfiniteScroll
+          dataLength={fetchedArtCount}
+          next={() => fetchNextPage()}
+          hasMore={!!hasNextPage}
+          loader={<Spinner />}
         >
-          {data &&
-            data.pages.map((page, index) => (
-              <React.Fragment key={index}>
-                {page._embedded.artworks.map((art) => (
-                  <GridItem key={art.id}>
-                    <AppCard
-                      title={art.title}
-                      image={art._links.thumbnail.href}
-                      handleClick={() => onArtClick(art)}
-                    />
-                  </GridItem>
-                ))}
-              </React.Fragment>
-            ))}
-        </Grid>
-        <Box width={'100vw'} display={'flex'} justifyContent={'center'}>
-          <Button
-            justifySelf={'center'}
-            disabled={isFetchingNextPage}
-            onClick={() => fetchNextPage()}
+          <Grid
+            overflowY={'scroll'}
+            templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+            templateRows="repeat(auto-fill, minmax(auto, 1fr))"
+            columnGap={'3.5%'}
+            rowGap={5}
+            justifyItems="center"
+            padding={'3%'}
           >
-            {isFetchingNextPage ? 'Loading...' : 'Load more'}
-          </Button>
-        </Box>
+            {isLoading && skeletons.map((skeleton) => <CardSkeleton key={skeleton.id} />)}
+
+            {data &&
+              data.pages.map((page, index) => (
+                <React.Fragment key={index}>
+                  {page._embedded.artworks.map((art) => (
+                    <GridItem key={art.id}>
+                      <AppCard
+                        title={art.title}
+                        image={art._links.thumbnail.href}
+                        handleClick={() => onArtClick(art)}
+                      />
+                    </GridItem>
+                  ))}
+                </React.Fragment>
+              ))}
+          </Grid>
+        </InfiniteScroll>
       </Box>
     </>
   );
